15239e0fa716a58a67a55e0316783702330248e3
Edvaldo Afonso <eadafonso@gmail.com>
2022-04-12 07:25:32 +0100

    chore: make crud for classroom

--- src/services/courses.service.ts
+++ src/services/courses.service.ts
@@ -13,4 +13,33 @@ export class CoursesService {
   listAllCourses() {
     return this.prisma.course.findMany();
   }
+
+  getCourseById(id: string) {
+    return this.prisma.course.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async createCourse({ title }: CreateCourseParams) {
+    const slug = slugify(title, { lower: true });
+
+    const courseAlreadyExists = await this.prisma.course.findUnique({
+      where: {
+        slug,
+      },
+    });
+
+    if (courseAlreadyExists) {
+      throw new Error('Course already exists');
+    }
+
+    return this.prisma.course.create({
+      data: {
+        title,
+        slug,
+      },
+    });
+  }
 }
